0f02b2b84c948cb64c1bc9c3c590a57255510ced
Juan Sanchez <juan.sanchez@alphacredit.co>
2021-02-24 17:44:28 -0500

    Implementación unit test

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -1,27 +1,58 @@
 import { Injectable } from '@angular/core';
 
-interface productoCarrito {
+export interface productoCarrito {
   NombreProducto: string;
   Imagen: string;
   cantidad: number;
+  valor: number;
+  iva: number;
+}
+
+export interface factura {
+  valorTotal: number;
+  ivaTotal: number;
+  cantidadProductos: number;
 }
 @Injectable({
   providedIn: 'root',
 })
 export class CarritoService {
-  itemsCart: Array<productoCarrito> = new Array<productoCarrito>();
+  private productosEnCarrito: Array<productoCarrito> = new Array<productoCarrito>();
+  private factura: factura = {
+    valorTotal: 0,
+    ivaTotal: 0,
+    cantidadProductos: 0,
+  };
   constructor() {}
 
   añadirAlCarrito(producto: productoCarrito) {
-    var productoRecuperado = this.itemsCart.find(
+    var productoRecuperado = this.productosEnCarrito.find(
       (productoEnCarrito) =>
         productoEnCarrito.NombreProducto == producto.NombreProducto
     );
 
+    this.factura.valorTotal += producto.valor;
+    this.factura.ivaTotal += producto.iva;
+    this.factura.cantidadProductos += producto.cantidad;
+
     if (productoRecuperado === undefined || productoRecuperado === null) {
-      this.itemsCart.push(producto);
+      this.productosEnCarrito.push(producto);
     } else {
-      productoRecuperado.cantidad += 1;
+      productoRecuperado.cantidad += producto.cantidad;
+      productoRecuperado.iva += producto.iva;
+      productoRecuperado.valor += producto.valor;
     }
+  }
+  getValorFactura(): number {
+    return this.factura.valorTotal;
+  }
+  getIvaFactura(): number {
+    return this.factura.ivaTotal;
+  }
+  getTotalFactura(): number {
+    return this.factura.cantidadProductos;
+  }
+  getCarrito(): productoCarrito[] {
+    return this.productosEnCarrito;
   }
 }
